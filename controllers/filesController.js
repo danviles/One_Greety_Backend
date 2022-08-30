165452ded9ef56bf63b406850f9671233a3acb06
Elvis Nogueiras <danvilapps@gmail.com>
2022-08-30 21:59:39 +0100

    probando imagenes mobile

--- controllers/filesController.js
+++ controllers/filesController.js
@@ -12,12 +12,14 @@ const uploadImage = async (req, res) => {
 
 const editImage = async (req, res) => {
   console.log(req.body.imgId)
-  await cloudinary.v2.uploader.destroy(req.body.imgId);
-  const result = await cloudinary.v2.uploader.upload(req.file.path);
-  await fs.unlink(req.file.path);
+  console.log(req.file)
+  // await cloudinary.v2.uploader.destroy(req.body.imgId);
+  // const result = await cloudinary.v2.uploader.upload(req.file.path);
+  // await fs.unlink(req.file.path);
   res.send({
-    url: result.url.replace(/http/g, "https"),
-    publicId: result.public_id
+    // url: result.url.replace(/http/g, "https"),
+    // publicId: result.public_id
+    msg: 'ok'
   });
 }
 
